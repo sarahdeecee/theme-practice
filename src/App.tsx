42d8f05b83f6f5d7ba3b9a883e1439814d984f44
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-07-20 13:11:26 -0400

    add ColorContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 
 import { Button, createTheme, CssBaseline, PaletteMode, ThemeProvider } from "@mui/material";
 import { useMemo, useState } from "react";
+import { ColorContext } from "./ColorContext";
 import { darkTheme } from "./themes/dark";
 import { lightTheme } from "./themes/light";
 import { appTheme } from "./themes/theme";
 function App() {
   const [mode, setMode] = useState<PaletteMode>("light");
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode: PaletteMode) =>
+          prevMode === "light" ? "dark" : "light"
+        );
+      },
+    }),
+    []
+  );
 
   const theme = useMemo(
     () => createTheme(mode === "light" ? lightTheme : darkTheme),
@@ -13,9 +24,11 @@ function App() {
   );
 
   return (
-    <ThemeProvider theme={appTheme}>
-      <CssBaseline enableColorScheme />
-    </ThemeProvider>
+    <ColorContext.Provider value={colorMode}>
+      <ThemeProvider theme={appTheme}>
+        <CssBaseline enableColorScheme />
+      </ThemeProvider>
+    </ColorContext.Provider>
   );
 }
 export default App;
---
+++ src/ColorContext.ts
@@ -0,0 +1,7 @@
+import React from "react";
+interface ColorContextSchema {
+  toggleColorMode: () => void;
+};
+export const ColorContext = React.createContext<ColorContextSchema>(
+  {} as ColorContextSchema
+);
