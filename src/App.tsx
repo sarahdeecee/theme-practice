93dc2065da42f65b1098ff4eeb54bdc3a555d046
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-07-20 13:04:32 -0400

    fix code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 
-import { ThemeProvider } from "@mui/material";
+import { Button, CssBaseline, ThemeProvider } from "@mui/material";
 import { appTheme } from "./themes/theme";
-​
 function App() {
   return (
     <ThemeProvider theme={appTheme}>
-      <></>
+      <CssBaseline enableColorScheme />
+      <div style={{ margin: "1em" }}>
+        <Button color="primary" variant="contained">
+          Primary
+        </Button>
+        <Button color="secondary" variant="contained">
+          Secondary
+        </Button>
+      </div>
     </ThemeProvider>
   );
 }
-​
 export default App;
--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -1,6 +1,5 @@
 import { blue, pink } from "@mui/material/colors";
 import { createTheme } from "@mui/material/styles";
-​
 export const appTheme = createTheme({
   palette: {
     primary: blue,
