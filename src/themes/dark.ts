42f768dee245d4b4188f48208db63ce689ef21ee
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-07-20 13:05:57 -0400

    create light and dark theme files

---
+++ src/themes/dark.ts
@@ -0,0 +1,7 @@
+import { ThemeOptions } from "@mui/material";
+​
+export const darkTheme: ThemeOptions = {
+  palette: {
+    mode: "dark",
+  },
+};
---
+++ src/themes/light.ts
@@ -0,0 +1,7 @@
+import { ThemeOptions } from "@mui/material";
+​
+export const lightTheme: ThemeOptions = {
+  palette: {
+    mode: "light",
+  },
+};
